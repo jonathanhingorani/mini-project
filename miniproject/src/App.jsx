69ca691e3053e412fb45b87ceb3410f26d1030be
Guilherme Fontes <guilherme.fontes98@outlook.com>
2024-05-29 15:59:15 +0100

    first

--- miniproject/src/App.jsx
+++ miniproject/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import "./components/NavBar";
+import "./components/Footer";
+import "./components/SideBar";
 
 function App() {
   const [count, setCount] = useState(0);
